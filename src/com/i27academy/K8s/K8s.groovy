6d8e350f9ccdbcef60f8bc43e21b9d2d40351c8f
rdoonabo <dravikumar@gmail.com>
2025-03-08 18:03:38 +0530

    k8s auth login method

---
+++ src/com/i27academy/K8s/K8s.groovy
@@ -0,0 +1,16 @@
+package com.i27academy.K8s
+
+class K8s {
+    def jenkins
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+   def auth_login() {
+    jenkins.sh """#!/bin/bash
+    echo "Enterting the Authencation method for GKE cluster login"
+    gcloud config set account jenkins@final-devops-project-445009.iam.gserviceaccount.com
+    gcloud compute instances list
+   }
+    
+  }
