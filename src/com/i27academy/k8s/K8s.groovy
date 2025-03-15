a3ec50048da21620bfcd0ca75df6687226525ae9
rdoonabo <dravikumar@gmail.com>
2025-03-15 14:54:10 +0530

    added chargpt class file

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -1,55 +1,50 @@
+// K8s.groovy (in a shared library or script)
 package com.i27academy.k8s
 
 class K8s {
     def jenkins
+    
     K8s(jenkins) {
         this.jenkins = jenkins
     }
 
-   def auth_login(gke_name, gke_zone, gke_project) {
-    jenkins.sh """#!/bin/bash
-    echo "Enterting the Authencation method for GKE cluster login"
-    gcloud config set account jenkins@final-devops-project-445009.iam.gserviceaccount.com
-    gcloud compute instances list
-    echo "successfully "
-    gcloud container clusters get-credentials $gke_name --zone $gke_zone --project $gke_project
-    
-    kubectl get nodes
-    kubectl get pods
-    echo "get kube config file successfully"
-    """
-   }
+    def auth_login(gke_name, gke_zone, gke_project) {
+        jenkins.sh """#!/bin/bash
+        echo "Enterting the Authentication method for GKE cluster login"
+        gcloud config set account jenkins@final-devops-project-445009.iam.gserviceaccount.com
+        gcloud compute instances list
+        echo "Successfully authenticated"
+        gcloud container clusters get-credentials $gke_name --zone $gke_zone --project $gke_project
 
+        kubectl get nodes
+        kubectl get pods
+        echo "Kube config file fetched successfully"
+        """
+    }
 
-  def k8sdeploy(fileName,docker_image,namespace) {
-    jenkins.sh """#!/bin/bash 
-    echo "Excuitng the K8s Deploy Method"
-    echo "Final tag is $docker_image"
-    sed -i "s|DIT|$docker_image|g" ./.cicd/$fileName
-    kubectl apply -f ./.cicd/$fileName -n $namespace
-    echo "deploy k8s-dev done successfully"
-    """
-  }  
-  }
+    def k8sdeploy(fileName, docker_image, namespace) {
+        jenkins.sh """#!/bin/bash 
+        echo "Executing the K8s Deploy Method"
+        echo "Final tag is $docker_image"
+        sed -i "s|DIT|$docker_image|g" ./.cicd/$fileName
+        kubectl apply -f ./.cicd/$fileName -n $namespace
+        echo "K8s deploy done successfully"
+        """
+    }
 
-  def HelmChartDeploy(appName, env, helmChartPath) {
-   jenkins.sh """#!/bin/bash
-   
-   echo "*************** entering to Helm Repo ***************"
-   helm install ${appName}-${env}-chart -f ./.cicd/k8s/values_${env}.yaml  ${helmChartPath}
-   # helm install chartname -f valuefilepath chartpath
-   # helm upgrade chartname -f valuefilepath chartpath
-   """
-  }
+    def helmChartDeploy(appName, env, helmChartPath) {
+        jenkins.sh """#!/bin/bash
+        echo "*************** Entering Helm Repo ***************"
+        helm install ${appName}-${env}-chart -f ./.cicd/k8s/values_${env}.yaml  ${helmChartPath}
+        """
+    }
 
-  def gitClone() {
-    jenkins.sh """#!/bin/bash
-   
-   echo "*************** gitclone my shared lib ***************"
-   git clone https://github.com/rdoonabo/shared-lib-ravi.git
-   ls -la
-   ls -la  shared-lib-ravi/chart
-   """
-
-  }
-
+    def gitClone() {
+        jenkins.sh """#!/bin/bash
+        echo "*************** Cloning my shared lib ***************"
+        git clone https://github.com/rdoonabo/shared-lib-ravi.git
+        ls -la
+        ls -la shared-lib-ravi/chart
+        """
+    }
+}
